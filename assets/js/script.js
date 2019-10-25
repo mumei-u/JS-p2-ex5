5480b599255998ae88d212d557f78cea715e3aeb
thyago_lbt <thyago14@outlook.fr>
2019-10-25 09:47:09 +0200

    fin

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function img1() {
+  document.getElementById('image1').src="assets/img/image1_2.jpg";
+}
+function img2() {
+  document.getElementById('image2').src="assets/img/image2_2.jpg";
+}
+function img3() {
+  document.getElementById('image3').src="assets/img/image3_2.jpg";
+}
+function img4() {
+  document.getElementById('image4').src="assets/img/image4_2.jpg";
+}
+function img5() {
+  document.getElementById('image5').src="assets/img/image5_2.jpg";
+}
